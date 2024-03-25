5c00c952399b5e333df4765ac0c0955f3161c277
Tumoji <133051676+Tumoji@users.noreply.github.com>
2024-03-26 00:43:07 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 
 export const getIsLoading = state => state.contacts.isLoading;
@@ -5,3 +7,12 @@ export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 
 export const getStatusFilter = state => state.filter;
+
+export const getFilteredContacts = createSelector(
+  [getContacts, getStatusFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
